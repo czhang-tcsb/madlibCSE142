9556a851a9f078be1460ffd0d9374ec686b6ca8c
Colin Zhang <colinz@Colins-MBP.domain>
2019-04-03 15:59:06 -0700

    madlib git push

---
+++ madlibAssignment/madlib.py
@@ -0,0 +1,10 @@
+import os
+'''
+print(os.path.isfile('./test.txt'))
+print("This is madlib.py and i hate tacos ")
+'''
+#print("WHATs uP
+print("Welcome to the game of Mad Libs.")
+print("I will ask you to provide various words")
+print("and phrases to fill in a story.")
+print("The result will be written to an output file.")
