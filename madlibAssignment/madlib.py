16de5c2823b1236db9784e9fad6c336d1b3f8944
Colin Zhang <colinz@Colins-MBP.domain>
2019-04-24 15:15:40 -0700

    committed

--- madlibAssignment/madlib.py
+++ madlibAssignment/madlib.py
@@ -3,8 +3,27 @@ import os
 print(os.path.isfile('./test.txt'))
 print("This is madlib.py and i hate tacos ")
 '''
-#print("WHATs uP
+#intro message
 print("Welcome to the game of Mad Libs.")
 print("I will ask you to provide various words")
 print("and phrases to fill in a story.")
 print("The result will be written to an output file.")
+
+
+
+def CVQinput():
+    CVQ = input("(C)reate mad-lib, (V)iew mad-lib, (Q)uit? ")
+    print("This is CVQ: " + CVQ)
+    CVQ.lower()
+    print("This is CVQ: " + CVQ)
+    if(CVQ == "c"):
+        print("create function activated")
+    if(CVQ == "v"):
+        print("view function activated")
+        print(tarzan.txt)
+    if(CVQ == "q"):
+        print("quit activated")
+    while (CVQ != "c" and CVQ != "v" and CVQ != "q"):
+        CVQ = input("(C)reate mad-lib, (V)iew mad-lib, (Q)uit? ")
+        print("This is CVQ: " + CVQ)
+CVQinput()
