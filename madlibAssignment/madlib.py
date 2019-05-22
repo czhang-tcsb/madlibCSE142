ad32c5d23767d26490e64e8d225441c80523926e
Colin Zhang <colinz@Colins-MBP.domain>
2019-05-22 16:01:22 -0700

    hi

--- madlibAssignment/madlib.py
+++ madlibAssignment/madlib.py
@@ -8,29 +8,34 @@ print("Welcome to the game of Mad Libs.")
 print("I will ask you to provide various words")
 print("and phrases to fill in a story.")
 print("The result will be written to an output file.")
-GFN = 0
+GFN = ""
 #madlibBool = 1
+
 
 def CVQinput():
     madlibBool = 1
     while(madlibBool == 1):
         CVQ = input("(C)reate mad-lib, (V)iew mad-lib, (Q)uit? ")
-        print("This is CVQ: " + CVQ)
+        #print("This is CVQ: " + CVQ)
         CVQ = CVQ.lower()
-        print("This is CVQ after lowercased: " + CVQ)
+        #print("This is CVQ after lowercased: " + CVQ)
         while (CVQ != "c" and CVQ != "v" and CVQ != "q"):
             CVQ = input("(C)reate mad-lib, (V)iew mad-lib, (Q)uit? ")
-            print("This is CVQ: " + CVQ)
+        #    print("This is CVQ: " + CVQ)
+
         if(CVQ == "c"):
             print("create function activated")
             #getFileName()
         if(CVQ == "v"):
             print("view function activated")
-            #viewOperation()
+            viewOperation()
         if(CVQ == "q"):
             print("quit activated")
             madlibBool = 0
+
 def getFileName():
+    global GFN
+    print("GFN before assingment: " + GFN)
     GFN = input("Input file name ")
     print("GFN: " + GFN)
     print("this will show us if file exists")
@@ -45,9 +50,19 @@ def viewOperation():
     readFile()
 
 def readFile():
-
+    global GFN
+    print("This is GFN " + str(GFN))
+    fileToRead = GFN
+    fileReader = open(fileToRead)
+    lines = fileReader.readlines()
+    print(lines)
+    for x in range(0, len(lines)):
+        print(lines[x])
+    fileReader.close()
     '''
-    if(i == "clothes.txt"):
+    if(input == "clothes.txt"):
+        GFN = 1
+    if(GFN == 1):
         fileToRead = "clothes.txt"
         fileReader = open(fileToRead)
         lines = fileReader.readlines()
@@ -55,7 +70,9 @@ def readFile():
         for x in range(0, len(lines)):
             print(lines[x])
         fileReader.close()
-    elif(i == "dance.txt"):
+    if(input == "dances.txt"):
+        GFN = 2
+    elif(GFN == 2):
         fileToRead = "dance.txt"
         fileReader = open(fileToRead)
         lines = fileReader.readlines()
@@ -63,7 +80,9 @@ def readFile():
         for x in range(0, len(lines)):
             print(lines[x])
         fileReader.close()
-    elif(i == "simple.txt"):
+    if(input == "simple.txt"):
+        GFN = 3
+    elif(GFN == 3):
         fileToRead = "simple.txt"
         fileReader = open(fileToRead)
         lines = fileReader.readlines()
@@ -71,7 +90,9 @@ def readFile():
         for x in range(0, len(lines)):
             print(lines[x])
         fileReader.close()
-    elif(i == "tarzan.txt"):
+    if(input == "tarzan.txt"):
+        GFN = 4
+    elif(GFN == 4):
         fileToRead = "tarzan.txt"
         fileReader = open(fileToRead)
         lines = fileReader.readlines()
